68358e8881572ffe4b02221fdb17332c5e236faf
AnthCoding <anth.coding@users.noreply.github.com>
2021-10-17 16:30:46 -0400

    First app Change

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,9 @@
 import { FunctionComponent } from 'preact'
-import { Logo } from './logo'
 
 export const App: FunctionComponent = () => {
 	return (
 		<>
-			<Logo />
-			<p>Hello Vite + Preact!</p>
-			<p>
-				<a
-					class="link"
-					href="https://preactjs.com/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn Preact
-				</a>
-			</p>
+			<p>Initial App</p>
 		</>
 	)
 }
